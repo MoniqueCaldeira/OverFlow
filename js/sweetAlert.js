931989d857b167756850dae22716ab4cc3907cda
Monique Caldeira <nique.sca@gmail.com>
2023-10-01 19:12:21 -0300

    Melhorias no tutorial, caso o usuário não queira ver o tutorial ele pode clicar em sair e o PopUp fecha

--- js/sweetAlert.js
+++ js/sweetAlert.js
@@ -52,8 +52,8 @@ function tutorial() {
       title: "Tutorial",
       imageUrl: "../assets/tutorial01.png",
       imageWidth: 300,
-      text:"Para começar, pressione o botão 'Pop'.",
-      showCloseButton: false,
+      text: "Para começar, pressione o botão 'Pop'.",
+      showCloseButton: true,
       showCancelButton: true,
       focusConfirm: false,
       confirmButtonText:
@@ -61,24 +61,12 @@ function tutorial() {
       cancelButtonText:
          '<p>Sair</p>',
    }).then((result) => {
-      Swal.fire({
-         title: "Tutorial",
-         imageUrl: "../assets/tutorial03.png",
-         imageWidth: 300,
-         text:"Ao pressionar o botão, a primeira bola irá cair.",
-         showCloseButton: false,
-         showCancelButton: true,
-         focusConfirm: false,
-         confirmButtonText:
-            '<p>Próximo</p>',
-         cancelButtonText:
-            '<p>Sair</p>',
-      }).then((result) => {
+      if (result.isConfirmed) {
          Swal.fire({
             title: "Tutorial",
-            imageUrl: "../assets/tutorial02.png",
+            imageUrl: "../assets/tutorial03.png",
             imageWidth: 300,
-            text:"Segure a bola com o botão esquerdo do mouse e solte-a na Pilha. Mas lembre-se que você só podera mover a Bola que está no Topo da Pilha!",
+            text: "Ao pressionar o botão, a primeira bola irá cair.",
             showCloseButton: false,
             showCancelButton: true,
             focusConfirm: false,
@@ -87,24 +75,12 @@ function tutorial() {
             cancelButtonText:
                '<p>Sair</p>',
          }).then((result) => {
-            Swal.fire({
-               title: "Tutorial",
-               imageUrl: "../assets/tutorial04.png",
-               imageWidth: 300,
-               text:"Utilize as estruturas de Pilha para mover as Bolas e reorganiza-las como desejar. Mas cuidado para não ultrapassar a capacidade de armazenamento causando um 'OverFlow'",
-               showCloseButton: false,
-               showCancelButton: true,
-               focusConfirm: false,
-               confirmButtonText:
-                  '<p>Próximo</p>',
-               cancelButtonText:
-                  '<p>Sair</p>',
-            }).then((result) => {
+            if (result.isConfirmed) {
                Swal.fire({
                   title: "Tutorial",
-                  imageUrl: "../assets/tutorial05.png",
+                  imageUrl: "../assets/tutorial02.png",
                   imageWidth: 300,
-                  text:"Coloque as bolas na ordem correta nas Maquina 'Push'.",
+                  text: "Segure a bola com o botão esquerdo do mouse e solte-a na Pilha. Mas lembre-se que você só podera mover a Bola que está no Topo da Pilha!",
                   showCloseButton: false,
                   showCancelButton: true,
                   focusConfirm: false,
@@ -113,24 +89,63 @@ function tutorial() {
                   cancelButtonText:
                      '<p>Sair</p>',
                }).then((result) => {
-                  Swal.fire({
-                     title: "Tutorial",
-                     imageUrl: "../assets/tutorial06.png",
-                     imageWidth: 300,
-                     text:"Você terminará os níveis quando todas as Bolas estiverem na Máquina 'Push'.",
-                     showCloseButton: false,
-                     focusConfirm: false,
-                     confirmButtonText:
-                        '<p>Jogar</p>',
-                   
-                  })
-                  
+                  if (result.isConfirmed) {
+                     Swal.fire({
+                        title: "Tutorial",
+                        imageUrl: "../assets/tutorial04.png",
+                        imageWidth: 300,
+                        text: "Utilize as estruturas de Pilha para mover as Bolas e reorganiza-las como desejar. Mas cuidado para não ultrapassar a capacidade de armazenamento causando um 'OverFlow'",
+                        showCloseButton: false,
+                        showCancelButton: true,
+                        focusConfirm: false,
+                        confirmButtonText:
+                           '<p>Próximo</p>',
+                        cancelButtonText:
+                           '<p>Sair</p>',
+                     }).then((result) => {
+                        if (result.isConfirmed) {
+                           Swal.fire({
+                              title: "Tutorial",
+                              imageUrl: "../assets/tutorial05.png",
+                              imageWidth: 300,
+                              text: "Coloque as bolas na ordem correta nas Maquina 'Push'.",
+                              showCloseButton: false,
+                              showCancelButton: true,
+                              focusConfirm: false,
+                              confirmButtonText:
+                                 '<p>Próximo</p>',
+                              cancelButtonText:
+                                 '<p>Sair</p>',
+                           }).then((result) => {
+                              if (result.isConfirmed) {
+                                 Swal.fire({
+                                    title: "Tutorial",
+                                    imageUrl: "../assets/tutorial06.png",
+                                    imageWidth: 300,
+                                    text: "Você terminará os níveis quando todas as Bolas estiverem na Máquina 'Push'.",
+                                    showCloseButton: false,
+                                    focusConfirm: false,
+                                    confirmButtonText:
+                                       '<p>Jogar</p>',
+
+                                 })
+                              }
+
+
+                           })
+                        }
+
+
+                     })
+                  }
+
+
                })
-               
-            })
-            
+            }
+
+
          })
-         
-      })
+      }
+
    })
 }
